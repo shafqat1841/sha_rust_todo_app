aa8015aa4f64d697d3c7ed67790ddc4d2e059efe
shafqat1841 <shafqat1841@gmail.com>
2026-03-10 02:35:26 +0500

    Implement Todo and Todos structs; enhance viewing and adding todos functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
 use std::io;
 
+struct Todo {
+    id: u32,
+    description: String,
+    completed: bool,
+}
+
+struct Todos {
+    list: Vec<Todo>,
+}
+
 pub fn run() {
+    let mut todos = Todos { list: Vec::new() };
     let mut initial_text = "Welcome to the todo app!";
     loop {
         println!("{}", initial_text);
@@ -27,8 +38,33 @@ pub fn run() {
         println!("You entered: {}", input.trim().to_lowercase());
 
         match input.trim().to_lowercase().as_str() {
-            "v" => println!("Viewing all todos..."),
-            "a" => println!("Adding a todo..."),
+            "v" => {
+                if todos.list.len() == 0 {
+                    println!("No todos found! Please add some todos to view them.");
+                    continue;
+                }
+                println!("Viewing all todos...");
+                println!("Total todos: {}", todos.list.len());
+                for todo in &todos.list {
+                    println!(
+                        "ID: {}, Description: {}, Completed: {}",
+                        todo.id, todo.description, todo.completed
+                    );
+                }
+            }
+            "a" => {
+                
+                println!("Adding a todo...");
+                let last_id: u32 = match todos.list.last() {
+                    Some(todo) => {
+                        let new_id = todo.id + 1;    
+                        new_id
+                    }
+                    None => {
+                        1
+                    }
+                };
+            }
             "d" => println!("Deleting a todo..."),
             "u" => println!("Updating a todo..."),
             "q" => {
