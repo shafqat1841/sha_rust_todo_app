4bf519d1fc8e00611a7fbf94ae9e0d282fd61ecf
shafqat1841 <shafqat1841@gmail.com>
2026-03-10 02:08:03 +0500

    Refactor run function to display menu options and handle user input

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,32 @@
-use std::{thread::sleep, time::Duration};
+use std::io;
 
 pub fn run() {
+    let mut initial_text = "Welcome to the todo app!";
     loop {
-        println!("Hello, world2222!");
+        println!("{}", initial_text);
+        println!("To view all todos, press 'v'.");
+        println!("To add a todo, press 'a'.");
+        println!("To delete a todo, press 'd'.");
+        println!("To update a todo, press 'u'.");
+        println!("To exit, press 'q'.");
 
-        sleep(Duration::from_secs(5));
+        let mut input: String = String::new(); // Create a string variable
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                if n == 2 {
+                    initial_text = "You entered an empty command! Please add a command.";
+                    continue;
+                }
+            }
+            Err(error) => {
+                println!("Error reading input: {}", error);
+                continue; // Skip the rest of the loop and start over
+            }
+        };
+
+        println!("You entered: {}", input.trim());
+
+        // sleep(Duration::from_secs(5));
         break;
     }
 }
