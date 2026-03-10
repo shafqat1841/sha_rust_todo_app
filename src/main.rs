8bf733a173396e1464c1a88b4c1ef338d52fe99a
shafqat1841 <shafqat1841@gmail.com>
2026-03-11 02:25:09 +0500

    Refactor main function to handle errors and improve application stability

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use std::process;
 
 use todo_app::run;
 
-fn main() {
-    run();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    if let Err(e) = run() {
+        eprintln!("Error running app: {}", e);
+        process::exit(1);
+    }
+    Ok(())
 }
