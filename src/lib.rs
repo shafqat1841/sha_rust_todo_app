01adc00d4afa8a9c926f95ffec78247805a0f108
shafqat1841 <shafqat1841@gmail.com>
2026-03-11 02:24:55 +0500

    Refactor run function to initialize FileHandler and streamline input handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,80 +1,16 @@
-use std::io;
+mod file_handler_system;
+mod helper;
 
-struct Todo {
-    id: u32,
-    description: String,
-    completed: bool,
+use crate::file_handler_system::FileHandler;
+use crate::helper::aaaaa;
+
+
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file_handler = FileHandler::new();
+    file_handler.open_file()?;
+
+    aaaaa();
+
+    Ok(())
 }
 
-struct Todos {
-    list: Vec<Todo>,
-}
-
-pub fn run() {
-    let mut todos = Todos { list: Vec::new() };
-    let mut initial_text = "Welcome to the todo app!";
-    loop {
-        println!("{}", initial_text);
-        println!("To view all todos, press 'v'.");
-        println!("To add a todo, press 'a'.");
-        println!("To delete a todo, press 'd'.");
-        println!("To update a todo, press 'u'.");
-        println!("To exit, press 'q'.");
-
-        let mut input: String = String::new(); // Create a string variable
-        match io::stdin().read_line(&mut input) {
-            Ok(n) => {
-                if n == 2 {
-                    initial_text = "You entered an empty command! Please add a command.";
-                    continue;
-                }
-            }
-            Err(error) => {
-                println!("Error reading input: {}", error);
-                continue; // Skip the rest of the loop and start over
-            }
-        };
-
-        println!("You entered: {}", input.trim().to_lowercase());
-
-        match input.trim().to_lowercase().as_str() {
-            "v" => {
-                if todos.list.len() == 0 {
-                    println!("No todos found! Please add some todos to view them.");
-                    continue;
-                }
-                println!("Viewing all todos...");
-                println!("Total todos: {}", todos.list.len());
-                for todo in &todos.list {
-                    println!(
-                        "ID: {}, Description: {}, Completed: {}",
-                        todo.id, todo.description, todo.completed
-                    );
-                }
-            }
-            "a" => {
-                
-                println!("Adding a todo...");
-                let last_id: u32 = match todos.list.last() {
-                    Some(todo) => {
-                        let new_id = todo.id + 1;    
-                        new_id
-                    }
-                    None => {
-                        1
-                    }
-                };
-            }
-            "d" => println!("Deleting a todo..."),
-            "u" => println!("Updating a todo..."),
-            "q" => {
-                println!("Exiting the app. Goodbye!");
-                break;
-            }
-            _ => {
-                initial_text = "Invalid command! Please try again.";
-                continue;
-            }
-        }
-    }
-}
