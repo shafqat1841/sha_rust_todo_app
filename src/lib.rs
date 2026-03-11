300a08b8bb124814fb86228fe6021be9672ce42c
shafqat1841 <shafqat1841@gmail.com>
2026-03-12 03:59:43 +0500

    test the serialization and deserialization in lib file

--- src/lib.rs
+++ src/lib.rs
@@ -2,15 +2,23 @@ mod file_handler_system;
 mod helper;
 
 use crate::file_handler_system::FileHandler;
-use crate::helper::aaaaa;
-
+use crate::helper::{aaaaa, serde_deserialize, serde_serialize, serde_serialize_file};
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut file_handler = FileHandler::new();
     file_handler.open_file()?;
 
-    aaaaa();
+    let file = file_handler.get_file()?;
+
+    println!("File is ready to use: {:?}", file);
+
+    serde_serialize_file(file)?;
+
+    serde_serialize()?;
+
+    serde_deserialize();
+
+    // aaaaa();
 
     Ok(())
 }
-
