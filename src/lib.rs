55a21ede7d889a4dcc410f501bc3ea3020e03dc9
shafqat1841 <shafqat1841@gmail.com>
2026-03-13 03:09:55 +0500

    comment all other staff add file_test function for testing

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,31 @@
 mod file_handler_system;
 mod helper;
+mod file_handle_test;
+mod app_constants;
 
 use crate::file_handler_system::FileHandler;
 use crate::helper::{aaaaa, serde_deserialize, serde_serialize, serde_serialize_file};
+use crate::file_handle_test::file_test;
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file_handler = FileHandler::new();
-    file_handler.open_file()?;
+    // let mut file_handler = FileHandler::new();
+    // file_handler.open_file()?;
 
-    let file = file_handler.get_file()?;
+    // let file = file_handler.get_file()?;
 
-    println!("File is ready to use: {:?}", file);
+    // println!("File is ready to use: {:?}", file);
 
-    serde_serialize_file(file)?;
+    // serde_serialize_file(file)?;
 
-    serde_serialize()?;
+    // serde_serialize()?;
 
-    serde_deserialize();
+    // serde_deserialize();
 
     // aaaaa();
+
+    file_test();
+
+
 
     Ok(())
 }
