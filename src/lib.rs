f47cec5d07cbb29601fe50961b1dcad0c8ab0ca2
shafqat1841 <shafqat1841@gmail.com>
2026-03-16 02:33:19 +0500

    refactor logic in lib file

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,64 @@
-mod file_handler_system;
-mod helper;
-mod file_handle_test;
 mod app_constants;
 mod app_default_texts;
+mod file_handle_test;
+mod file_handler_system;
+mod helper;
 
-use crate::file_handler_system::FileHandler;
+use std::io;
+
 use crate::app_default_texts::DefaultTexts;
-// use crate::helper::{aaaaa, serde_deserialize, serde_serialize, serde_serialize_file};
+use crate::file_handler_system::FileHandler;
+// use crate::helper::{ serde_deserialize, serde_serialize, serde_serialize_file};
 // use crate::file_handle_test::file_test;
 
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    let mut file_handler = FileHandler::new()?;
-    let default_texts = DefaultTexts::new();
+    let file_handler = FileHandler::new()?;
+    let mut default_texts = DefaultTexts::new();
+
 
     loop {
-        println!("{}", default_texts.get_initial_text());
-        break;
+        if !default_texts.initial_text_viewed {
+            println!("{}", default_texts.get_initial_text());
+        }
+
+        let mut input: String = String::new(); // Create a string variable
+        match io::stdin().read_line(&mut input) {
+            Ok(n) => {
+                if n == 2 {
+                    println!("{}", default_texts.show_empty_command_text());
+                    continue;
+                }
+            }
+            Err(error) => {
+                println!("Error reading input: {}", error);
+                continue; // Skip the rest of the loop and start over
+            }
+        };
+
+        match input.trim().to_lowercase().as_str() {
+            "v" => {
+                file_handler.view_all_todos();
+            }
+            "a" => {
+                file_handler.add_todo();
+            }
+            "d" => {
+                file_handler.delete_todo();
+            }
+            "u" => {
+                file_handler.update_todo();
+            }
+            "q" => {
+                // println!("Exiting the app. Goodbye!");
+                file_handler.quit();
+                break;
+            }
+            _ => {
+                println!("{}", default_texts.get_invalid_command_text());
+                continue;
+            }
+        }
     }
 
     // let file = file_handler.get_file()?;
@@ -32,8 +74,6 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     // aaaaa();
 
     // file_test();
-
-
 
     Ok(())
 }
