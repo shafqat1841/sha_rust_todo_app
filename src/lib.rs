ff3a2134be0b8e57efa3e64e8befb71189b45ffc
shafqat1841 <shafqat1841@gmail.com>
2026-03-16 02:04:17 +0500

    refactor ther lib file for the new FileHandler and DefaultTexts updates

--- src/lib.rs
+++ src/lib.rs
@@ -2,14 +2,22 @@ mod file_handler_system;
 mod helper;
 mod file_handle_test;
 mod app_constants;
+mod app_default_texts;
 
 use crate::file_handler_system::FileHandler;
-use crate::helper::{aaaaa, serde_deserialize, serde_serialize, serde_serialize_file};
-use crate::file_handle_test::file_test;
+use crate::app_default_texts::DefaultTexts;
+// use crate::helper::{aaaaa, serde_deserialize, serde_serialize, serde_serialize_file};
+// use crate::file_handle_test::file_test;
+
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
-    // let mut file_handler = FileHandler::new();
-    // file_handler.open_file()?;
+    let mut file_handler = FileHandler::new()?;
+    let default_texts = DefaultTexts::new();
+
+    loop {
+        println!("{}", default_texts.get_initial_text());
+        break;
+    }
 
     // let file = file_handler.get_file()?;
 
@@ -23,7 +31,7 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
 
     // aaaaa();
 
-    file_test();
+    // file_test();
 
 
 
