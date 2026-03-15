e4a8e99bcd305cf67dd3a89bfc717c3a70485a9c
shafqat1841 <shafqat1841@gmail.com>
2026-03-16 02:32:38 +0500

    add new methods for FileHandler

--- src/file_handler_system.rs
+++ src/file_handler_system.rs
@@ -20,12 +20,56 @@ impl FileHandler {
             .create(true)
             .open(file_path)?;
 
-            Ok(
-                
-                FileHandler {
-                    file,
-                    file_path,
-                }
-            )
+        Ok(FileHandler { file, file_path })
+    }
+
+    pub fn view_all_todos(&self) {
+        // if todos.list.len() == 0 {
+        //     println!("");
+        //     println!("No todos found! Please add some todos to view them.");
+        //     println!("");
+        //     continue;
+        // }
+        // println!("Viewing all todos...");
+        // println!("Total todos: {}", todos.list.len());
+        // for todo in &todos.list {
+        //     println!(
+        //         "ID: {}, Description: {}, Completed: {}",
+        //         todo.id, todo.description, todo.completed
+        //     );
+        // }
+        println!("Viewing all todos...");
+    }
+
+    pub fn add_todo(&self) {
+        //  println!("Adding a todo...");
+        //             let last_id: u32 = match todos.list.last() {
+        //                 Some(todo) => {
+        //                     let new_id = todo.id + 1;
+        //                     new_id
+        //                 }
+        //                 None => 1,
+        //             };
+
+        //             let new_todo = Todo {
+        //                 id: last_id,
+        //                 description: format!("Todo {}", last_id),
+        //                 completed: false,
+        //             };
+
+        //             todos.list.push(new_todo);
+        println!("Adding a todo...");
+    }
+
+    pub fn delete_todo(&self) {
+        println!("Deleting a todo...");
+    }
+
+    pub fn update_todo(&self) {
+        println!("Updating a todo...");
+    }
+
+    pub fn quit(&self) {
+        println!("Exiting the app. Goodbye!");
     }
 }
