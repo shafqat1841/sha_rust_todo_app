a2206b55e8946b4cc032280967505ab74fe5a2b8
shafqat1841 <shafqat1841@gmail.com>
2026-03-17 02:52:30 +0500

    implement new method for todo and todos and also add add_new_todo method for todos

--- src/todos_system.rs
+++ src/todos_system.rs
@@ -7,7 +7,35 @@ pub struct Todo {
     completed: bool,
 }
 
+impl Todo {
+    fn new(id: u32, description: String) -> Self {
+        Todo {
+            id,
+            description,
+            completed: false,
+        }
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Todos {
+    todo_number: u32,
     list: Vec<Todo>,
+}
+
+impl Todos {
+    pub fn new() -> Self {
+        let new_todos = Todos {
+            todo_number: 0,
+            list: Vec::new(),
+        };
+
+        return new_todos;
+    }
+
+    pub fn add_new_todo(&mut self, description: String) {
+        self.todo_number = self.todo_number + 1;
+        let new_todo = Todo::new(self.todo_number, description);
+        self.list.push(new_todo) 
+    }
 }
