a4b8e86f8a37d3ea6f47140fb6bbfa17c4f433f1
shafqat1841 <shafqat1841@gmail.com>
2026-03-17 02:50:41 +0500

    update FileHandler new method logic and add initial logic for adding new todo in FileHandler

--- src/file_handler_system.rs
+++ src/file_handler_system.rs
@@ -1,13 +1,14 @@
 use std::{
-    fs::{File, OpenOptions},
-    io::BufReader,
+    fs::{self, File, Metadata, OpenOptions},
+    io::{self, BufReader, BufWriter},
     path::Path,
 };
 
-use crate::{app_constants::FILE_PATH, todos_system::Todos};
+use crate::{app_constants::FILE_PATH, helper::get_user_input, todos_system::Todos};
 
 pub struct FileHandler {
     file: File,
+    file_path: &'static Path,
 }
 
 impl FileHandler {
@@ -19,7 +20,24 @@ impl FileHandler {
             .create(true)
             .open(file_path)?;
 
-        Ok(FileHandler { file })
+        let metadata = file.metadata()?;
+
+        if metadata.len() == 0 {
+            let temp_path = file_path.with_extension("temp");
+            let temp_file = File::create(&temp_path)?;
+
+            let mut writer = BufWriter::new(temp_file);
+
+            let json_value = Todos::new();
+
+            serde_json::to_writer_pretty(&mut writer, &json_value)?;
+
+            writer.into_inner()?.sync_all()?;
+
+            fs::rename(&temp_path, file_path)?;
+        }
+
+        Ok(FileHandler { file, file_path })
     }
 
     pub fn view_all_todos(&self) -> Result<(), Box<dyn std::error::Error>> {
@@ -31,7 +49,7 @@ impl FileHandler {
         }
 
         let reader = BufReader::new(&self.file);
-        let todo_data: Vec<Todos> = serde_json::from_reader(reader)?;
+        let todo_data: Todos = serde_json::from_reader(reader)?;
 
         println!("All todos are the following:");
         println!("{:#?}", todo_data);
@@ -39,6 +57,82 @@ impl FileHandler {
     }
 
     pub fn add_todo(&self) {
+        let mut initial_text_viewed = false;
+        loop {
+            if !initial_text_viewed {
+                println!("Please enter a description for the new todo:");
+                println!("Or type 'cancel' to go back:");
+                initial_text_viewed = true
+            }
+            let description = get_user_input();
+
+            if description.is_err() {
+                let description_ref = description.as_ref().err().unwrap();
+                match description_ref.kind() {
+                    io::ErrorKind::InvalidInput => {
+                        println!(
+                            "Empty description entered. Please provide a valid description for the todo."
+                        );
+                        println!("Or type 'cancel' to go back.");
+                    }
+                    io::ErrorKind::Other => println!("Error reading input: {}", description_ref),
+                    _ => println!("An unexpected error occurred: {}", description_ref),
+                }
+                continue;
+            }
+
+            let reader = BufReader::new(&self.file);
+            let mut todo_data: serde_json::Result<Todos> = serde_json::from_reader(reader);
+
+            if todo_data.is_err() {
+                println!("Something went wront. Please try again");
+                continue;
+            }
+
+            let mut todo_data_unwrap = todo_data.unwrap();
+
+            todo_data_unwrap.add_new_todo(description.unwrap());
+
+            let temp_path = self.file_path.with_extension("temp");
+            let temp_file = File::create(&temp_path);
+
+            if temp_file.is_err() {
+                println!("Something went wront. Please try again");
+                continue;
+            }
+
+            let mut writer = BufWriter::new(temp_file.unwrap());
+
+            let json_value = todo_data_unwrap;
+
+            let serde_json_return = serde_json::to_writer_pretty(&mut writer, &json_value);
+
+            if serde_json_return.is_err() {
+                println!("Something went wront. Please try again");
+                continue;
+            }
+
+            let into_inner_value = writer.into_inner();
+
+            if into_inner_value.is_err() {
+                println!("Something went wront. Please try again");
+                continue;
+            }
+
+            let sync_all_value = into_inner_value.unwrap().sync_all();
+
+            if sync_all_value.is_err() {
+                println!("Something went wront. Please try again");
+                continue;
+            }
+
+            let rename_value = fs::rename(&temp_path, self.file_path);
+
+            if rename_value.is_err() {
+                println!("Something went wront. Please try again");
+                continue;
+            }
+        }
         //  println!("Adding a todo...");
         //             let last_id: u32 = match todos.list.last() {
         //                 Some(todo) => {
