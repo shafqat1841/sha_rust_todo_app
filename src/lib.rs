3c0f178189ffc46cafbd4f0c4f97d26fa5acba5e
shafqat1841 <shafqat1841@gmail.com>
2026-03-22 04:01:47 +0500

    remove commented out code

--- src/lib.rs
+++ src/lib.rs
@@ -9,8 +9,6 @@ use std::io;
 use crate::app_default_texts::DefaultTexts;
 use crate::file_handler_system::FileHandler;
 use crate::helper::get_user_input;
-// use crate::helper::{ serde_deserialize, serde_serialize, serde_serialize_file};
-// use crate::file_handle_test::file_test;
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut file_handler = FileHandler::new()?;
@@ -62,7 +60,6 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
                 }
             }
             "q" => {
-                // println!("Exiting the app. Goodbye!");
                 file_handler.quit();
                 break;
             }
@@ -72,20 +69,6 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
             }
         }
     }
-
-    // let file = file_handler.get_file()?;
-
-    // println!("File is ready to use: {:?}", file);
-
-    // serde_serialize_file(file)?;
-
-    // serde_serialize()?;
-
-    // serde_deserialize();
-
-    // aaaaa();
-
-    // file_test();
 
     Ok(())
 }
