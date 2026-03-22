fa6d118fbb568c701f9e417d544c50c41d0f53da
shafqat1841 <shafqat1841@gmail.com>
2026-03-23 04:56:35 +0500

    refactor the code in lib.rs file to use AppCommand for clearity and other things

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,14 @@
 mod app_constants;
+mod commands;
 mod file_handler_system;
 mod helper;
 mod todos_system;
 
 use std::io;
 
+use crate::commands::AppCommands;
 use crate::file_handler_system::FileHandler;
-use crate::helper::{
-    get_default_texts, get_initial_text, get_invalid_command_text, get_user_input,
-    show_empty_command_text,
-};
+use crate::helper::{get_default_texts, get_initial_text, get_user_input, show_empty_command_text};
 
 pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     let mut file_handler = FileHandler::new()?;
@@ -18,57 +17,62 @@ pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     loop {
         let input = get_user_input();
 
-        if input.is_err() {
-            let input_ref = input.as_ref().err().unwrap();
-            match input_ref.kind() {
-                io::ErrorKind::InvalidInput => {
-                    println!("{}", show_empty_command_text())
+        let input_string = match input {
+            Ok(val) => val,
+            Err(e) => {
+                match e.kind() {
+                    io::ErrorKind::InvalidInput => {
+                        println!("{}", show_empty_command_text())
+                    }
+                    io::ErrorKind::Other => println!("Error reading input: {}", e),
+                    _ => println!("An unexpected error occurred: {}", e),
                 }
-                io::ErrorKind::Other => println!("Error reading input: {}", input_ref),
-                _ => println!("An unexpected error occurred: {}", input_ref),
+                continue;
             }
-            continue;
-        }
+        };
 
-        match input.unwrap().trim().to_lowercase().as_str() {
-            "v" => {
+        let command: AppCommands = match input_string.parse() {
+            Err(err) => {
+                println!("{}", err);
+                println!("{}", get_default_texts());
+
+                continue;
+            }
+            Ok(cmd) => cmd,
+        };
+
+        match command {
+            AppCommands::View => {
                 let res = file_handler.view_all_todos();
                 if res.is_err() {
                     println!("following error occured: {:?}", res.err())
                 }
-                println!("{}",get_default_texts());
             }
-            "a" => {
+            AppCommands::Add => {
                 let res = file_handler.add_todo();
                 if res.is_err() {
                     println!("following error occured: {:?}", res.err())
                 }
-                 println!("{}",get_default_texts());
             }
-            "d" => {
+            AppCommands::Delete => {
                 let res = file_handler.delete_todo();
                 if res.is_err() {
                     println!("following error occured: {:?}", res.err())
                 }
-                 println!("{}",get_default_texts());
             }
-            "u" => {
+            AppCommands::Update => {
                 let res = file_handler.update_todo();
 
                 if res.is_err() {
                     println!("following error occured: {:?}", res.err())
                 }
-                 println!("{}",get_default_texts());
             }
-            "q" => {
+            AppCommands::Quit => {
                 println!("Exiting the app. Goodbye!");
                 break;
             }
-            _ => {
-                println!("{}", get_invalid_command_text());
-                continue;
-            }
         }
+        println!("{}", get_default_texts());
     }
 
     Ok(())
