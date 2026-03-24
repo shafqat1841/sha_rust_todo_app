aac08f93c5f73c817a45f26a8b37b8e39b93d9b0
shafqat1841 <shafqat1841@gmail.com>
2026-03-24 20:25:06 +0500

    remove unwanted show_empty_command_text helper function

--- src/helper.rs
+++ src/helper.rs
@@ -1,7 +1,11 @@
 use std::io;
 
-use crate::app_constants::{
-    ADD_TODO_TEXT, EMPTY_COMMAND_TEXT, INVALID_COMMAND, QUIT_TEXT, REMOVE_TODO_TEXT, UPDATE_TODO_DONE_TEXT, VIEW_ALL_TODOS_TEXT, WELCOME_TEXT
+use crate::{
+    app_constants::{
+        ADD_TODO_TEXT, EMPTY_COMMAND_TEXT, INVALID_COMMAND, QUIT_TEXT, REMOVE_TODO_TEXT,
+        UPDATE_TODO_DONE_TEXT, VIEW_ALL_TODOS_TEXT, WELCOME_TEXT,
+    },
+    todos_erros::TodosErrors,
 };
 
 pub fn get_default_texts() -> String {
@@ -15,35 +19,22 @@ pub fn get_initial_text() -> String {
     format!("{}\n{}\n", WELCOME_TEXT, get_default_texts(),)
 }
 
-pub fn show_empty_command_text() -> String {
-    format!("{}\n{}\n", EMPTY_COMMAND_TEXT, get_default_texts(),)
-}
-
 pub fn get_invalid_command_text() -> String {
     format!("{}\n{}\n", INVALID_COMMAND, get_default_texts())
 }
 
-pub fn get_user_input() -> io::Result<String> {
+pub fn get_user_input() -> Result<String, TodosErrors> {
     let mut input: String = String::new();
     let input_data = io::stdin().read_line(&mut input);
 
     let input_data_ref = input_data.as_ref();
 
     if input_data.is_err() {
-        let new_error = io::Error::new(
-            io::ErrorKind::Other,
-            format!("Error reading input: {}", input_data.err().unwrap()),
-        );
-        let err = Err(new_error);
-        return err;
+        return Err(TodosErrors::IOError(input_data.err().unwrap()));
     }
 
     if input_data_ref.is_ok() && *input_data_ref.unwrap() == 2 {
-        let err = Err(io::Error::new(
-            io::ErrorKind::InvalidInput,
-            "Empty command entered",
-        ));
-        return err;
+        return Err(TodosErrors::EmptyCommandError);
     }
 
     Ok(input.trim().to_string())
